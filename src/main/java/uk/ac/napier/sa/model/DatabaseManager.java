d263e77cadceace56eb01f61eb6e039ca4c85726
refracc <40345422@live.napier.ac.uk>
2021-10-12 10:30:44 +0100

    Added documentation to DatabaseManager.java

--- src/main/java/uk/ac/napier/sa/model/DatabaseManager.java
+++ src/main/java/uk/ac/napier/sa/model/DatabaseManager.java
@@ -1,5 +1,7 @@
 package uk.ac.napier.sa.model;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.sql.*;
 
 public final class DatabaseManager {
@@ -10,6 +12,11 @@ public final class DatabaseManager {
     private DatabaseManager() {
     }
 
+    /**
+     * A thread-safe way of instantiating the {@link DatabaseManager} class.
+     *
+     * @return An instance of this class.
+     */
     public synchronized static DatabaseManager getInstance() {
         if (instance == null) {
             synchronized (DatabaseManager.class) {
@@ -21,7 +28,17 @@ public final class DatabaseManager {
         return instance;
     }
 
-    public boolean connect(String host, int port, String database, boolean useSSL, String user, String pass) {
+    /**
+     * Create a connection to the database.
+     * @param host The host address of the machine.
+     * @param port The port allocated to the SQL database.
+     * @param database The database to be used for the connection.
+     * @param useSSL Whether to use SSL for the connection.
+     * @param user The username.
+     * @param pass The password.
+     * @return True if the database can be connected to, otherwise false.
+     */
+    public boolean connect(@NotNull String host, int port, @NotNull String database, boolean useSSL, @NotNull String user, @NotNull String pass) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -39,6 +56,10 @@ public final class DatabaseManager {
         return false;
     }
 
+    /**
+     * Disconnect from the database.
+     * @return True if disconnect is successful, false if connection did not exist in the first place.
+     */
     public boolean disconnect() {
         if (conn != null) {
             try {
@@ -51,7 +72,11 @@ public final class DatabaseManager {
         return false;
     }
 
-    public ResultSet query(String sql) {
+    /**
+     * Query the database.
+     * @param sql The sql statement being used to query the database.
+     */
+    public ResultSet query(@NotNull String sql) {
         try {
             PreparedStatement stmt = conn.prepareStatement(sql);
             return stmt.executeQuery();
@@ -61,7 +86,14 @@ public final class DatabaseManager {
         return null;
     }
 
-    public boolean updateCustomer(int id, String name, boolean loyal) {
+    /**
+     * Method used to update a customer by ID.
+     * @param id The unique identifier allocated to all Customers.
+     * @param name The name of the customer
+     * @param loyal If the customer holds a valid loyalty card
+     * @return True if the update is successful.
+     */
+    public boolean updateCustomer(int id, @NotNull String name, boolean loyal) {
         try {
             PreparedStatement stmt = conn.prepareStatement(
                     "UPDATE customer SET name = ?, loyal = ? WHERE id = " + id + ";"
@@ -76,7 +108,15 @@ public final class DatabaseManager {
         return false;
     }
 
-    public boolean updateProduct(int id, String name, int stock, double price) {
+    /**
+     * The method used to update a product by ID
+     * @param id The unique identifier of the product.
+     * @param name The name of the product.
+     * @param stock The stock quantity.
+     * @param price The price of the item.
+     * @return True if the update is successful.
+     */
+    public boolean updateProduct(int id, @NotNull String name, int stock, double price) {
         try {
             PreparedStatement stmt = conn.prepareStatement(
                     "UPDATE product SET name = ?, stock = ?, price = ? WHERE id = " + id + ";"
@@ -92,6 +132,13 @@ public final class DatabaseManager {
         return false;
     }
 
+    /**
+     * The method used to update a sale.
+     * @param id The unique identifier of the sale.
+     * @param product The product purchased.
+     * @param type The type of sale.
+     * @return True if the sale is updated successfully, false otherwise.
+     */
     public boolean updateSale(int id, int product, int type) {
         try {
             PreparedStatement stmt = conn.prepareStatement(
@@ -107,7 +154,18 @@ public final class DatabaseManager {
         return false;
     }
 
-    public boolean updateTransaction(int id, int product, int customer, int sale, double cost, Timestamp time) {
+
+    /**
+     * The method used for udpateing a transaction.
+     * @param id The unique identifier for the transaction.
+     * @param product The purchased product's ID
+     * @param customer The customer's ID
+     * @param sale The sale type.
+     * @param cost The total cost of the transaction.
+     * @param time The date & time this transaction occurred.
+     * @return True if the transaction has been updated accordingly, false otherwise.
+     */
+    public boolean updateTransaction(int id, int product, int customer, int sale, double cost, @NotNull Timestamp time) {
         try {
             PreparedStatement stmt = conn.prepareStatement(
                     "UPDATE transaction SET product = ?, customer = ?, sale = ?, cost = ?, purchased = ? WHERE ID = " + id + ";"
