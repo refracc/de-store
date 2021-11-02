14ae35cbb14b9be3e63ca0251a67cd2e71908390
Stewart Anderson <40345422@live.napier.ac.uk>
2021-11-02 12:14:09 +0000

    Converted Product.java to Product.kt

--- src/main/java/uk/ac/napier/sa/controller/adt/Product.kt
+++ src/main/java/uk/ac/napier/sa/controller/adt/Product.kt
@@ -1,24 +1,45 @@
-package uk.ac.napier.sa.controller.adt;
+package uk.ac.napier.sa.controller.adt
 
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.text.DecimalFormat
+import java.math.RoundingMode
+import java.lang.StringBuilder
+import java.util.ArrayList
 
 /**
  * This class is used to represent all products in a ORM-based fashion.
  */
-public class Product {
+class Product {
+    /**
+     * Obtain the product ID number.
+     *
+     * @return The product ID.
+     */
+    private var id = 0
 
-    private int id;
-    private String name;
-    private int quantity;
-    private double price;
-    private List<Integer> sales;
+    /**
+     * Obtain the product name.
+     *
+     * @return The product name.
+     */
+    private var name: String? = null
 
-    private Product() {
-    }
+    /**
+     * Obtain the product quantity
+     *
+     * @return The quantity of the product
+     */
+    private var quantity = 0
+
+    /**
+     * Obtain the price per unit
+     *
+     * @return The price per unit
+     */
+    private var price = 0.0
+
+    private var sales: List<Int>? = null
+
+    private constructor() {}
 
     /**
      * The default constructor for this class.
@@ -29,48 +50,12 @@ public class Product {
      * @param price    The price of the item.
      * @param sales    The list of sales (containing the sales type integer) with this item.
      */
-    public Product(int id, String name, int quantity, double price, List<Integer> sales) {
-        this.id = id;
-        this.name = name;
-        this.quantity = quantity;
-        this.price = price;
-        this.sales = ((sales == null) ? new ArrayList<>() : sales);
-    }
-
-    /**
-     * Obtain the product ID number.
-     *
-     * @return The product ID.
-     */
-    public int getId() {
-        return id;
-    }
-
-    /**
-     * Obtain the product name.
-     *
-     * @return The product name.
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * Obtain the product quantity
-     *
-     * @return The quantity of the product
-     */
-    public int getQuantity() {
-        return quantity;
-    }
-
-    /**
-     * Obtain the price per unit
-     *
-     * @return The price per unit
-     */
-    public double getPrice() {
-        return price;
+    constructor(id: Int, name: String?, quantity: Int, price: Double, sales: List<Int?>?) {
+        this.id = id
+        this.name = name
+        this.quantity = quantity
+        this.price = price
+        this.sales = (sales ?: ArrayList()) as List<Int>?
     }
 
     /**
@@ -78,22 +63,22 @@ public class Product {
      *
      * @return A copy of the list of sale types.
      */
-    public String getSales() {
-        return Arrays.toString(sales.toArray());
+    private fun getSales(): String {
+        return sales!!.toTypedArray().contentToString()
     }
 
-    @Override
-    public String toString() {
-        DecimalFormat df = new DecimalFormat("#.##");
-        df.setRoundingMode(RoundingMode.UP);
-        return String.format("""
-                        Product (ID): %d
-                        Product (Name): %s
-                        Product (Price): £%s/unit
-                        Product (Stock): %d
-                        Product (Sales): %s
-                        """,
-                this.getId(), this.getName(), df.format(this.getPrice()), this.getQuantity(),
-                this.getSales().contains("1") ? "3 for 2" : this.getSales().contains("2") ? "Buy One Get One Free" : this.getSales().contains("3") ? "Free Delivery" : "N/A");
+    override fun toString(): String {
+        val df = DecimalFormat("#.##")
+        df.roundingMode = RoundingMode.UP
+        return String.format(
+            StringBuilder().append("Product (ID): %d\n").append("Product (Name): %s\n")
+                .append("Product (Price): £%s/unit\n").append("Product (Stock): %d\n").append("Product (Sales): %s\n")
+                .toString(),
+            id, name, df.format(price), quantity,
+            if (getSales().contains("1")) "3 for 2" else if (getSales().contains("2")) "Buy One Get One Free" else if (getSales().contains(
+                    "3"
+                )
+            ) "Free Delivery" else "N/A"
+        )
     }
 }
